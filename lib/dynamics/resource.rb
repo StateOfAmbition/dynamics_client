74e8a775bd45e0e2afd7c98cc9790e7a73a3cb64
Alex Thomas <alexthomas@Alexs-MacBook-Pro.local>
2018-03-08 21:25:24 +0000

    Bug fix: make resource methods class methods

--- lib/dynamics/resource.rb
+++ lib/dynamics/resource.rb
@@ -36,6 +36,22 @@ module Dynamics
           Dynamics::Client::Base.get("#{resource_type}(#{id})", params)
         end
 
+        def resource_name
+          @resouce_name ||= "#{resource_prefix}_#{self.name.split('::').last.
+          gsub(/([A-Z]+)([A-Z][a-z])/,'\1_\2').
+          gsub(/([a-z\d])([A-Z])/,'\1_\2').
+          tr("-", "_").downcase}"
+        end
+
+        def resource_type
+          "#{resource_name}#{resource_plural}"
+        end
+
+        def resource_plural
+          match = /^(\w+)(?:y)$/i.match(resource_name)
+          match ? (match[1] + "ies") : (resource_name + "s")
+        end
+
       end
 
       private
@@ -58,22 +74,6 @@ module Dynamics
 
         def resource_prefix
           raise "Dynamics::Resource must implement #resource_prefix"
-        end
-
-        def resource_name
-          @resouce_name ||= "#{resource_prefix}_#{self.name.split('::').last.
-          gsub(/([A-Z]+)([A-Z][a-z])/,'\1_\2').
-          gsub(/([a-z\d])([A-Z])/,'\1_\2').
-          tr("-", "_").downcase}"
-        end
-
-        def resource_type
-          "#{resource_name}#{resource_plural}"
-        end
-
-        def resource_plural
-          match = /^(\w+)(?:y)$/i.match(resource_name)
-          match ? (match[1] + "ies") : (resource_name + "s")
         end
 
     end
