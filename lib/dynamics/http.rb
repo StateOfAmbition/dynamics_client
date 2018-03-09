d1f19c1d5a437c75f8756d70e3841127a552133f
Alex Thomas <alexthomas@Alexs-MacBook-Pro.local>
2018-03-09 11:53:00 +0000

    bug fix: document already parsed

--- lib/dynamics/http.rb
+++ lib/dynamics/http.rb
@@ -6,7 +6,7 @@ module Dynamics
       def parse(status, body)
         begin
           JSON.parse(body).tap do |document|
-            data = JSON.parse(document).has_key?(:value) ? document["value"] : document
+            data = document.has_key?(:value) ? document["value"] : document
             ::Api::Client::Response.new(status, data).tap do |r|
               puts "[API::Client] Response: status #{r.status} data: #{r.data.inspect}" if log_response?
             end
